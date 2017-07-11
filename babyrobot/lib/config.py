bba2f8d7497c241856a44d14af3dfb49c215e39b
George  Paraskevopoulos <georgepar.91@gmail.com>
2017-07-11 12:57:15 +0000

    Create a class to easily parse YAML config files

---
+++ babyrobot/lib/config.py
@@ -0,0 +1,51 @@
+import os
+import six
+
+import babyrobot.lib.utils as br_utils
+
+
+class Config(object):
+    """
+    Parse a YAML configuration into a python object.
+    The YAML properties will be stored in the Config class attributes.
+    For the example YAML file (e.g., config.yaml):
+
+    item1:
+      property1: value1
+      property2:
+        subproperty: value2
+
+    item2:
+      moduleproperty: value3
+
+    Usage:
+    from babyrobot.lib import config as br_config
+    CONF = br_config.Config(config_file='config.yaml')
+    print(CONF.item1.property1)
+    >>> value1
+    print(CONF.item1.property2.subproperty)
+    >>> value2
+    print(CONF.item2.moduleproperty)
+    >>> value3
+    """
+    def __init__(self, config_file=None, config_dict=None):
+        try:
+            self.config_file = (os.environ['BABYROBOT_CONFIG']
+                                if config_file is None else config_file)
+            if config_dict:
+                self._parse(None, config_dict)
+            else:
+                self.yaml_conf = br_utils.yaml2dict(self.config_file)
+                self._parse(None, self.yaml_conf)
+        except Exception as error:
+            raise Exception('Parse config failed: {}'.format(str(error)))
+
+    def _parse(self, attr_now, left_parametes):
+        for param_n, param_v in six.iteritems(left_parametes):
+            if isinstance(param_v, dict):
+                # Recurse creating a new object
+                self.__setattr__(
+                    param_n,
+                    Config(config_file=self.config_file, config_dict=param_v))
+            else:
+                self.__setattr__(param_n, param_v)
