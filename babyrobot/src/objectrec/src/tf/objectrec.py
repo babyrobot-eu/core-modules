b1d40d803526d539570df98ffc9624ca4b3d6565
Christos Baziotis <christos.baziotis@gmail.com>
2017-07-19 11:49:28 +0300

    Add yaml config and code refactoring
    Moved paths and settings from code to an external yaml file.
    Also, performed code refactoring.
    
    Signed-off-by: Christos Baziotis <christos.baziotis@gmail.com>

---
+++ babyrobot/src/objectrec/src/tf/objectrec.py
@@ -0,0 +1,56 @@
+import glob
+import os
+
+import tensorflow as tf
+from PIL import Image
+
+from babyrobot.lib.utils import get_parameter_from_yaml, yaml2dict
+from utils import load_frozen_model, check_model, orec_image, \
+    visualize_recognized_objects, debug_info_image, get_labels_map
+
+VISUALIZE = True
+##############################################################################
+# PATHS & FILENAMES
+##############################################################################
+SCRIPT_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# get_parameter_from_yaml()
+config = yaml2dict("config.yaml")
+
+# Images
+PATH_TO_TEST_IMAGES_DIR = os.path.join(SCRIPT_DIR_PATH, '..', 'images')
+TEST_IMAGE_PATHS = glob.glob(PATH_TO_TEST_IMAGES_DIR + "/*.jpg")
+
+# Download Model
+check_model(config["model"]["name"])
+
+# Load a (frozen) Tensorflow model into memory.
+detect_graph = load_frozen_model(config["model"]["name"])
+
+# Loading label map
+label_map = get_labels_map(config["model"]["labels"],
+                           config["model"]["classes"])
+
+with detect_graph.as_default():
+    with tf.Session(graph=detect_graph) as sess:
+        for image_path in TEST_IMAGE_PATHS:
+            image = Image.open(image_path)
+
+            # objects = orec_image(sess, detection_graph, image)
+            (boxes, scores, classes, num_detections) = orec_image(sess,
+                                                                  detect_graph,
+                                                                  image)
+            # boxes = [ymin, xmin, ymax, xmax]
+
+            # print debugging information
+            debug_info_image(image, boxes, scores, classes, label_map,
+                             config["model"]["threshold"])
+
+            # Visualization of the results of a detection.
+            if VISUALIZE:
+                visualize_recognized_objects(image,
+                                             boxes,
+                                             classes,
+                                             scores,
+                                             label_map,
+                                             config["model"]["threshold"])
--- babyrobot/src/objectrec/src/tf/utils.py
+++ babyrobot/src/objectrec/src/tf/utils.py
@@ -9,29 +9,54 @@ import numpy as np
 import six.moves.urllib as urllib
 import tensorflow as tf
 from scipy.spatial.distance import euclidean
+from tensorflow.models.object_detection.utils.label_map_util import \
+    convert_label_map_to_categories, load_labelmap, create_category_index
 
-from tensorflow.models.object_detection.utils import visualization_utils as vis_util
+from tensorflow.models.object_detection.utils import \
+    visualization_utils as vis_util
 
 
-def check_model(url, save_loc, extract_loc):
-    if not os.path.isfile(save_loc):
+def check_model(model_name):
+    """
+    Check if the given model exists on disk.
+    If it doesn't, then download it
+    :param model_name:
+    :return:
+    """
+    _model_file = model_name + '.tar.gz'
+    _url_base = 'http://download.tensorflow.org/models/object_detection/'
+    _script_loc = os.path.dirname(os.path.abspath(__file__))
+    _save_loc = os.path.join(_script_loc, 'models', _model_file)
+    _extract_loc = os.path.join(_script_loc, 'models')
+
+    if not os.path.isfile(_save_loc):
         print("Downloading Model...")
         opener = urllib.request.URLopener()
-        opener.retrieve(url)
-        tar_file = tarfile.open(save_loc)
-        for file in tar_file.getmembers():
-            file_name = os.path.basename(file.name)
+        opener.retrieve(_url_base + _model_file, _save_loc)
+        tar_file = tarfile.open(_save_loc)
+        for _file in tar_file.getmembers():
+            file_name = os.path.basename(_file.name)
             if 'frozen_inference_graph.pb' in file_name:
-                tar_file.extract(file, extract_loc)
+                tar_file.extract(_file, _extract_loc)
         print("done!")
 
 
-def load_frozen_model(model_path):
+def load_frozen_model(model_name):
+    """
+    Loads a pre-trained model from the disk
+    and returns a TF Graph object
+    :param model_name:
+    :return:
+    """
+    _script_loc = os.path.dirname(os.path.abspath(__file__))
+    _model_path = os.path.join(_script_loc, 'models', model_name,
+                               'frozen_inference_graph.pb')
+
     print("Loading a (frozen) Tensorflow model into memory...")
     detection_graph = tf.Graph()
     with detection_graph.as_default():
         od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(model_path, 'rb') as fid:
+        with tf.gfile.GFile(_model_path, 'rb') as fid:
             serialized_graph = fid.read()
             od_graph_def.ParseFromString(serialized_graph)
             tf.import_graph_def(od_graph_def, name='')
@@ -63,7 +88,37 @@ def distance_from_center(box, image):
 
 
 def capture_frame():
+    """
+    Captures a frame from the camera and returns it as an Image
+    * Note:
+    :return:
+    """
+
     pass
+
+
+def get_labels_map(labels, n_classes):
+    """
+    Label maps map indices to category names,
+    so that when our convolution network predicts 5,
+    we know that this corresponds to airplane.
+    Here we use internal utility functions,
+    but anything that returns a dictionary mapping integers
+    to appropriate string labels would be fine
+    :return:
+    """
+    _tf_models_dir = os.path.abspath(tf.models.object_detection.__file__) \
+        .strip("__init__.py").strip("__init__.pyc")
+
+    # List of the strings that is used to add correct label for each box.
+    _labels_path = os.path.join(_tf_models_dir, 'data', labels)
+
+    label_map = load_labelmap(_labels_path)
+    categories = convert_label_map_to_categories(label_map,
+                                                 max_num_classes=n_classes,
+                                                 use_display_name=True)
+    category_index = create_category_index(categories)
+    return category_index
 
 
 def orec_image(sess, comp_graph, image):
@@ -76,19 +131,25 @@ def orec_image(sess, comp_graph, image):
     :return: boxes, scores, classes, num_detections
     """
 
-    # the array based representation of the image will be used later in order to prepare the
-    # result image with boxes and labels on it.
+    # the array based representation of the image will be used later
+    # in order to prepare the result image with boxes and labels on it.
     image_np = load_image_into_numpy_array(image)
-    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+
+    # Expand dimensions since the model expects images to have
+    # shape: [1, None, None, 3]
     image_np_expanded = np.expand_dims(image_np, axis=0)
     image_tensor = comp_graph.get_tensor_by_name('image_tensor:0')
-    # Each box represents a part of the image where a particular object was detected.
+
+    # Each box represents a part of the image
+    # where a particular object was detected.
     boxes = comp_graph.get_tensor_by_name('detection_boxes:0')
+
     # Each score represent how level of confidence for each of the objects.
     # Score is shown on the result image, together with the class label.
     scores = comp_graph.get_tensor_by_name('detection_scores:0')
     classes = comp_graph.get_tensor_by_name('detection_classes:0')
     num_detections = comp_graph.get_tensor_by_name('num_detections:0')
+
     # Actual detection.
     (boxes, scores, classes, num_detections) = sess.run(
         [boxes, scores, classes, num_detections],
@@ -108,7 +169,8 @@ def orec_image(sess, comp_graph, image):
     return boxes, scores, classes, num_detections
 
 
-def visualize_recognized_objects(image, boxes, classes, scores, category_index, threshold):
+def visualize_recognized_objects(image, boxes, classes, scores, category_index,
+                                 threshold):
     """
 
     :param image:
@@ -121,8 +183,8 @@ def visualize_recognized_objects(image, boxes, classes, scores, category_index, 
     # Size, in inches, of the output images. For visualization
     IMAGE_SIZE = (12, 8)
 
-    w, h = image.size
-    my_dpi = 96  # todo: find this programmatically
+    # w, h = image.size
+    # my_dpi = 96  # todo: find this programmatically
 
     image_np = load_image_into_numpy_array(image)
     vis_util.visualize_boxes_and_labels_on_image_array(
@@ -148,7 +210,9 @@ def debug_info_image(image, boxes, scores, classes, category_index, threshold):
     # print("--------------------")
     # print(image_path)
     # print("--------------------")
-    for x, y, b in list(zip(np.squeeze(scores), np.squeeze(classes).astype(np.int32), np.squeeze(boxes))):
+    for x, y, b in list(zip(np.squeeze(scores),
+                            np.squeeze(classes).astype(np.int32),
+                            np.squeeze(boxes))):
         # for x, y, b in objects:
         if x > threshold:
             print(category_index[y]["name"], x, distance_from_center(b, image))
