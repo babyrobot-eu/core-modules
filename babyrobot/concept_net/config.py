ef26a509925e35f594167579f6a8d7a571af5d05
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2017-10-23 18:14:11 +0300

    Add conceptual network config file
    Signed-off-by: Giorgos Paraskevopoulos <geopar@central.ntua.gr>

---
+++ babyrobot/concept_net/config.py
@@ -0,0 +1,13 @@
+class ROS_CONFIG(object):
+    SERVICE_NAME = 'conceptual_networks'
+    SERVER_NODE = 'concept_net_server'
+    CLIENT_NODE = 'concet_net_client'
+
+
+class SEMANTIC_SPACE(object):
+    TEXT = ('/home/geopar/projects/babyrobot-integration/models/'
+            'semantic_spaces/text.json')
+    VISUAL = ('/home/geopar/projects/babyrobot-integration/models/'
+              'semantic_spaces/visual.json')
+    AUDIO = ('/home/geopar/projects/babyrobot-integration/models/'
+             'semantic_spaces/audio.json')
