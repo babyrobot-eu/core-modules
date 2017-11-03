29d6df38bbb0e67c7ab6990427c1a61e4fa663a1
Christos Baziotis <christos.baziotis@gmail.com>
2017-11-03 19:45:31 +0200

    minor path fixes
    Signed-off-by: Christos Baziotis <christos.baziotis@gmail.com>

--- babyrobot/src/emorec_pytorch/src/emorec_pytorch_client.py
+++ babyrobot/src/emorec_pytorch/src/emorec_pytorch_client.py
@@ -2,14 +2,13 @@
 
 import rospy
 
-from babyrobot.emorec import client as emorec_client
-from babyrobot.emorec import config as emorec_config
+from babyrobot.emorec_pytorch import client as emorec_client
 from babyrobot.speech_features import config as sf_config
 from babyrobot.lib.utils import mock_audio_segment
 
 
 if __name__ == "__main__":
-    rospy.init_node(emorec_config.ROS_CONFIG.CLIENT_NODE)
+    rospy.init_node("emorec_pytorch")
     clip = mock_audio_segment(sf_config.TEST.WAV_SAMPLE)
     recognized = emorec_client.emorec(clip)
     rospy.logerr("Service responded with {}".format(recognized))
