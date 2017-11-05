af7b3eaf5ef33180da644f79f8b909a9c6d9d182
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2017-11-05 20:46:35 +0200

    Try to run services automatically
    Signed-off-by: Giorgos Paraskevopoulos <geopar@central.ntua.gr>

--- dockerfiles/run_services.sh
+++ dockerfiles/run_services.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-tmux new-session -d -s services
-tmux new-window -t services:1 -n roscore 'bash -c "roscore"'
+/usr/bin/tmux new-session -d -s services
+/usr/bin/tmux new-window -t services:1 -n roscore 'bash -c "roscore"'
 sleep 4
-tmux new-window -t services:2 -n speech_features 'bash -c "roslaunch speech_features server.launch"'
+/usr/bin/tmux new-window -t services:2 -n speech_features 'bash -c "roslaunch speech_features server.launch"'
 sleep 4
-tmux new-window -t services:3 -n emorec 'bash -c "roslaunch emorec server.launch"'
+/usr/bin/tmux new-window -t services:3 -n emorec 'bash -c "roslaunch emorec server.launch"'
 sleep 4
-tmux new-window -t services:4 -n objectrec 'bash -c "roslaunch objectrec server.launch"'
+/usr/bin/tmux new-window -t services:4 -n objectrec 'bash -c "roslaunch objectrec server.launch"'
