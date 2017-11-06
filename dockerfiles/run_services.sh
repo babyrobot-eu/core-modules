cfb37f0744458bc53864af7949f08d62d3217cb4
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2017-11-06 13:34:28 +0200

    Fix run_services script
    Signed-off-by: Giorgos Paraskevopoulos <geopar@central.ntua.gr>

--- dockerfiles/run_services.sh
+++ dockerfiles/run_services.sh
@@ -3,8 +3,8 @@
 /usr/bin/tmux new-session -d -s services
 /usr/bin/tmux new-window -t services:1 -n roscore 'bash -c "roscore"'
 sleep 4
-/usr/bin/tmux new-window -t services:2 -n speech_features 'bash -c "roslaunch speech_features server.launch"'
+/usr/bin/tmux new-window -t services:2 -n speech_features 'bash -c "roslaunch --wait speech_features server.launch"'
 sleep 4
-/usr/bin/tmux new-window -t services:3 -n emorec 'bash -c "roslaunch emorec server.launch"'
+/usr/bin/tmux new-window -t services:3 -n emorec 'bash -c "roslaunch --wait emorec server.launch"'
 sleep 4
-/usr/bin/tmux new-window -t services:4 -n objectrec 'bash -c "roslaunch objectrec server.launch"'
+/usr/bin/tmux new-window -t services:4 -n objectrec 'bash -c "roslaunch --wait objectrec server.launch"'
