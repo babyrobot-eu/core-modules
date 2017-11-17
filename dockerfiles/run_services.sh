a2cb413704ebd54dc413b9f63ad158ba371ae630
George Paraskevopoulos <geopar@central.ntua.gr>
2017-11-17 11:26:50 +0000

    Add conceptual networks launch files in run_services
    Signed-off-by: George Paraskevopoulos <geopar@central.ntua.gr>

--- dockerfiles/run_services.sh
+++ dockerfiles/run_services.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-set -v
-
 /usr/bin/tmux new-session -d -s services
 /usr/bin/tmux new-window -t services:1 -n roscore 'bash -c "roscore"'
-sleep 4
+sleep 2
 /usr/bin/tmux new-window -t services:2 -n speech_features 'bash -c "roslaunch --wait speech_features server.launch"'
-sleep 4
+sleep 2
 /usr/bin/tmux new-window -t services:3 -n emorec 'bash -c "roslaunch --wait emorec server.launch"'
-sleep 4
+sleep 2
 /usr/bin/tmux new-window -t services:4 -n objectrec 'bash -c "roslaunch --wait objectrec server.launch"'
-sleep 4
+sleep 2
 /usr/bin/tmux new-window -t services:5 -n concept_space 'bash -c "roslaunch --wait concept_net concept_space_server.launch"'
-sleep 4
-/usr/bin/tmux new-window -t services:6 -n fusion 'bash -c "roslaunch --wait concept_net fusion_server.launch"'
-sleep 4
+sleep 2
+/usr/bin/tmux new-window -t services:6 -n semantic_fusion 'bash -c "roslaunch --wait concept_net fusion_server.launch"'
+sleep 2
 /usr/bin/tmux new-window -t services:7 -n semantic_similarity 'bash -c "roslaunch --wait concept_net semantic_similarity_server.launch"'
-sleep 4
